71bbc7131be9b0ee77ff3211bd39024b46e7375f
Simon Mayes <simon.mayes@bglgroup.co.uk>
2021-09-27 22:28:00 +0100

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,31 @@
 import React from "react";
 
 import { Form } from './components/Form'
-import { isEmailValid } from "./validators";
-import logo from "./logo.svg";
 import "./App.css";
 
-class App extends React.Component<{}, { input: string, isEmailValid: boolean}> {
+class App extends React.Component<{}, { input: string, isValid: boolean}> {
   constructor() {
     super({});
     this.state = { 
       input: "", 
-      isEmailValid: true,
+      isValid: false,
     };
   }
 
-  myChangeHandler = (event: any) => {
-    console.log(event.target.value)
-    this.setState({input: event.target.value});
-  }
-
-  mySubmitHandler = (event: any) => {
-    event.preventDefault();
-    this.setState({isEmailValid: isEmailValid(this.state.input)})
+  isValid = (valid: boolean) => {
+    this.setState({isValid: valid})
   }
 
   render() {
     return (
-      <div>
-        <div>
-          <div>
-            <Form onSubmit={this.mySubmitHandler} onChange={this.myChangeHandler}/>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Form isValid={this.isValid}/>
           </div>
           <div>
-            {this.state.isEmailValid ? null : <h3>Invalid input</h3> }
+            {this.state.isValid ? "Valid input" : <h3>Invalid input</h3> }
           </div>
-        </div>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
         </div>
       </div>
     );
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,56 @@
-export const Form = (props:any) => {
-  return (
-    <form onSubmit={props.onSubmit}>
-      <p>Enter your email, and submit:</p>
-      <input type="text" onChange={props.onChange} />
-      <input type="submit" />
-    </form>
-  );
+import React from "react";
+
+
+import FormBS from 'react-bootstrap/Form'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import { isEmailValid } from "../validators";
+
+type MyProps = {
+  // using `interface` is also ok
+  isValid: (valid: boolean) => void
 };
+type MyState = {
+  input: string; // like this
+  isEmailValid: boolean;
+};
+export class Form extends React.Component<MyProps,MyState> {
+
+  state : MyState = {
+    input: "",
+    isEmailValid: false
+  }
+
+  myChangeHandler = (event: any) => {
+    this.setState({input: event.target.value});
+    this.setState({isEmailValid: isEmailValid(this.state.input)})
+  }
+
+  mySubmitHandler = (event: any) => {
+    event.preventDefault();
+    this.setState({isEmailValid: isEmailValid(this.state.input)}, () => {
+      this.props.isValid(this.state.isEmailValid)
+    })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.mySubmitHandler}>
+        <p>Enter your email, and submit:</p>
+        <InputGroup className="mb-3">
+          <InputGroup.Text>Email</InputGroup.Text>
+          <FormControl onChange={this.myChangeHandler}
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+            name="email"
+            isValid={this.state.isEmailValid}
+            isInvalid={!this.state.isEmailValid}
+          />
+        </InputGroup>
+        <Button as="input" type="submit" value="Submit" />{' '}
+      </form>
+    );
+  }
+}
